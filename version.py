a2866acadc3de072c34c8b5518f5e84f1f868472
wxnacy <wxnacy@gmail.com>
2022-01-03 23:07:10 +0800

    U: 删除无用脚本

--- version.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding:utf-8 -*-
-# Author: wxnacy(wxnacy@gmail.com)
-# Description:
-
-from goss import __version__
-
-if __name__ == "__main__":
-    print(__version__.__version__)
